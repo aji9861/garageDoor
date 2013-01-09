f6b2eca66bb62e54b201663739756ea3f06c685d
Bill Beers <nothingatall544@gmail.com>
2013-01-09 18:44:40 -0500

    comments
    Signed-off-by: Bill Beers <nothingatall544@gmail.com>

--- DoorOpened.cpp
+++ DoorOpened.cpp
@@ -5,27 +5,35 @@
  *      Author: adam
  */
 
-#include "DoorOpened.h"
-#include "DoorLowering.h"
+#include "DoorOpened.h"		//used for state change
+#include "DoorLowering.h"	//same
 
-#include <iostream>
-
-using namespace std;
+#include <iostream>			//used to output to terminal
+using namespace std;		//same
 
 State* DoorOpened::acceptEvent(StateSignal s){
+	/* method called to receive signals,
+	 * returns:
+	 * signal == btn_push: DoorLowering state
+	 * signal == anything else: self
+	 */
 	if (s == btn_push){
 		return new DoorLowering;
 	}
-
 	return this;
 }
 
 StateSignal DoorOpened::onEntry(){
+	/* method called on entry to the state, declares that the state has been
+	 * entered.  return should not adjust state of the motor
+	 */
 	cout << "Enter door opened" << endl;
 	return no_signal;
 }
 
 StateSignal DoorOpened::onExit(){
-	//cout << "Exit door opened" << endl;
+	/* method called on exit to the state.  return should not adjust state of
+	 * the motor
+	 */
 	return no_signal;
 }
