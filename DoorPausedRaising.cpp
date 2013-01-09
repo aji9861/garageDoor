208f413d8b53e2a6a1e1258b6b4598eb29826b23
Bill Beers <nothingatall544@gmail.com>
2013-01-09 18:55:22 -0500

    more comments
    Signed-off-by: Bill Beers <nothingatall544@gmail.com>

--- DoorPausedRaising.cpp
+++ DoorPausedRaising.cpp
@@ -5,14 +5,17 @@
  *      Author: adam
  */
 
-#include "DoorPausedRaising.h"
-#include "DoorLowering.h"
-
-#include <iostream>
-
-using namespace std;
+#include "DoorPausedRaising.h"	//used for inheritance
+#include "DoorLowering.h"		//used for state change
+#include <iostream>				//used to output to terminal
+using namespace std;			//same
 
 State* DoorPausedRaising::acceptEvent(StateSignal s){
+	/* method called to receive signals,
+	 * returns:
+	 * signal == btn_push: DoorLowering state
+	 * signal == anything else: self
+	 */
 	if (s == btn_push){
 		return new DoorLowering;
 	}
@@ -20,11 +23,16 @@ State* DoorPausedRaising::acceptEvent(StateSignal s){
 }
 
 StateSignal DoorPausedRaising::onEntry(){
-	cout << "Enter door paused raising" << endl;
+	/* method called on entry to the state, declares that the state has been
+	 * entered.  return should not adjust state of the motor
+	 */
+	cout << "Enter door paused" << endl;
 	return no_signal;
 }
 
 StateSignal DoorPausedRaising::onExit(){
-	//cout << "Exit door paused raising" << endl;
+	/* method called on exit to the state.  return should not adjust state of
+	 * the motor
+	 */
 	return no_signal;
 }
--- DoorRaising.cpp
+++ DoorRaising.cpp
@@ -5,15 +5,20 @@
  *      Author: adam
  */
 
-#include "DoorRaising.h"
-#include "DoorPausedRaising.h"
-#include "DoorOpened.h"
-
-#include <iostream>
-
-using namespace std;
+#include "DoorRaising.h"		//used for inheritance
+#include "DoorPausedRaising.h"	//used for state change
+#include "DoorOpened.h"			//same
+#include <iostream>				//used to output to terminal
+using namespace std;			//same
 
 State* DoorRaising::acceptEvent(StateSignal s){
+	/* method called to receive signals,
+	 * returns:
+	 * signal == btn_push: DoorPausedRaising state
+	 * signal == motor_overcurrent: DoorPausedRaising state
+	 * signal == door_open: DoorOpened state
+	 * signal == anything else: self
+	 */
 	if (s == btn_push){
 		return new DoorPausedRaising;
 	}
@@ -28,11 +33,16 @@ State* DoorRaising::acceptEvent(StateSignal s){
 }
 
 StateSignal DoorRaising::onEntry(){
+	/* method called on entry to the state, declares that the state has been
+	 * entered.  return should make the door start raising
+	 */
 	cout << "Enter door raising" << endl;
 	return motor_up_active;
 }
 
 StateSignal DoorRaising::onExit(){
-	//cout << "Exit door raising" << endl;
+	/* method called on exit to the state.  return should make the motor stop
+	 * raising.
+	 */
 	return motor_up_inactive;
 }
