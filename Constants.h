83098c9397a85cfe85d3e9c29b32d4ecbffe7fef
Adam <aji9861@rit.edu>
2013-01-20 15:50:25 -0500

    Added comments to constants

--- Constants.h
+++ Constants.h
@@ -1,8 +1,16 @@
+// Port size for insializing
 #define IO_PORT_SIZE 1
+// base address for mapping registers
 #define CTRL_ADDRESS 0x280
 
+// Initialization value for the control register
 #define INIT_CTRL_REG 0x02
 
+// Address of the digitial register
 #define DIGITAL_REG 0x0B
+
+// offset from the base address for DIO_A
 #define DIO_A 0x08
+
+// offset from the base address for DIO_B
 #define DIO_B 0x09
